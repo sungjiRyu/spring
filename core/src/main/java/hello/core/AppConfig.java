2cb4986afe21f4c248d0626f536ca4e609c6b247
sungjiRyu <116089824+sungjiRyu@users.noreply.github.com>
2024-06-01 21:45:33 +0900

    컨테이너에 등록된 모든 빈 조회

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -28,7 +28,7 @@ public class AppConfig {
     }
     @Bean
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
-//        return new RateDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
